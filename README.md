f38dc14fe1153dca92bed4c4b88ce5ee11e0cfc0
Krisha Parmar <krishaparmar710@gmail.com>
2024-05-27 11:09:44 +0530

    create server

--- README.md
+++ README.md
@@ -6,7 +6,27 @@
 <br>
 <p>npm is the default package manager for the JavaScript runtime environment Node.js and is included as a recommended feature in the Node.js installer.</p>
 <br>
-<p> npm init => package.json =>details of projects</p>
+<p> npm init => package.json =>details of projects</p><br>
 
 # FileSystem methods
 <p>https://nodejs.org/docs/latest/api/fs.html#fswritefilefile-data-options-callback<br>https://nodejs.org/docs/latest/api/fs.html#fsappendfilepath-data-options-callback<br>https://nodejs.org/docs/latest/api/fs.html#fsrenameoldpath-newpath-callback<br>https://nodejs.org/docs/latest/api/fs.html#fscopyfilesrc-dest-mode-callback<br>https://nodejs.org/docs/latest/api/fs.html#fsunlinkpath-callback<br>https://nodejs.org/docs/latest/api/fs.html#fsrmpath-options-callback<br>https://nodejs.org/docs/latest/api/fs.html#fsmkdirpath-options-callback<br>https://nodejs.org/docs/latest/api/fs.html#fsreadfilepath-options-callback<br>https://nodejs.org/docs/latest/api/fs.html#fsreaddirpath-options-callback<br></p>
+
+# HTTP
+<h3>protocal => rules</h3>
+<br>
+<h3> http protocal <h3><br> <p>not possible any connection between user to server and server to user</p>
+<br>
+<h3>HTTP create server<br></h3>
+<p>https://nodejs.org/docs/latest/api/http.html#httpcreateserveroptions-requestlistener</p>
+<pre>const http = require('http');
+
+const server = http.createServer((req,res)=>{
+    res.end("hello world!");
+})
+
+server.listen(3000);</pre>
+
+
+
+
+
--- scripts.js
+++ scripts.js
@@ -1,56 +1,8 @@
-const fs = require('fs');
-// const { buffer } = require('stream/consumers');
-////////create file
-fs.writeFile("heyy.txt", "hello! How are you? ", function(err){
-    if(err) console.error(err);
-    else console.log("done")
+const http = require('http');
+
+const server = http.createServer((req,res)=>{
+    res.end("hello world!");
 })
-// fs.appendFile("hey.txt", " I am good  ", function(err){
-//     if(err) console.error(err);
-//     else console.log("done")
-// })
-// fs.rename("hey.txt","hello.txt",function(err){
-//     if(err) console.error(err);
-//     else console.log("done")
-// })
-// fs.copyFile("hello.txt","./copy/copy1.txt",function(err){
-//     if(err) console.error(err);
-//     else console.log("done")
-// })
-//throw an error
-// fs.copyFile("hello.txt","./copy2/copy1.txt",function(err){
-//     if(err) console.error(err.message);
-//     else console.log("done")
-// })
 
-// fs.unlink("hello.txt",function(err){
-//     if(err) console.error(err)
-//     else console.log("removed");
-// })
-
-// fs.rm("./copy",{recursive: true},(err)=>{
-//     if(err) console.error(err)
-//     else console.log("removed folder");
-// })
-
-////////////////make new folder 
-if(!fs.existsSync('./temp')){
-    fs.mkdirSync('./temp')
-}
-
-
-
-// // Create ./tmp/a/apple, regardless of whether ./tmp and ./tmp/a exist.
-fs.mkdir('./tmp/a/apple', { recursive: true }, (err) => {
-  if (err) throw err;
-});
-
-fs.readFile("heyy.txt",'utf8',(err, data)=>{
-    if(err) throw err;
-    else console.log(data);
-})
-fs.readdir("./temp",'utf8',(err, data)=>{
-    if(err) throw err;
-    else console.log(data);
-})
+server.listen(3000);
 
