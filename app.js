bc946d3b7e82e6b3f1c4f493634707d3ca1bdf10
Krisha Parmar <krishaparmar710@gmail.com>
2024-07-24 13:41:15 +0530

    verify and get

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
 const express = require('express');
 const app = express();
 const bcrypt = require('bcrypt');
-
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
 
 app.get("/",(req,res)=>{
-    bcrypt.genSalt(10, function(err, salt) {
-        bcrypt.hash("krishi", salt, function(err, hash) {
-            console.log(hash);
-        });
+    bcrypt.compare("krishi", "$2b$10$VViBFlw/MjEGcokfczWn1evjBaYdOuQhUZfyZdMROLrlwtaLXps4y", function(err, result) {
+       let token = jwt.sign({email:"krisha@gmail.com"},"secret");
+       res.cookie("token",token);
+       console.log(token);
+       res.send("done");
     });
     
 })
 
+app.get("/read", function (req,res) {
+    let data =jwt.verify(req.cookies.token,"secret");
+    console.log(data);
+})
 
 
 app.listen(3000);
-
-
-//hash->$2b$10$2fPppGL7ocZY3/HRMdwfS.BBm0Iz0CKI90T/eCEq5Ehqh0DyLvffO
