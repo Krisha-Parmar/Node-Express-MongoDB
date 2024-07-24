7acb47821891f12c4ea05634857ae2dd82e7d9b7
Krisha Parmar <krishaparmar710@gmail.com>
2024-07-24 17:20:37 +0530

    not use full

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.static(path.join(__dirname,'public')));
 app.use(cookieParser());
 
 app.get('/',(req,res)=>{
-    res.render('index');
+    res.render('index')
 });
 
 app.post('/create',(req,res)=>{
