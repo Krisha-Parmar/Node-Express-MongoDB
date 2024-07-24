0459e3c6d520d9a3e0fabc74910352d46cd25c1b
Krisha Parmar <krishaparmar710@gmail.com>
2024-07-24 13:32:41 +0530

    jwt

--- README.md
+++ README.md
@@ -184,6 +184,36 @@ app.get("/read",(req,res)=>{
 
 app.listen(3000);</pre>
 
+<br>
+<h3>data verify using jwt </h3>
+
+<pre>const express = require('express');
+const app = express();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
+app.get("/",(req,res)=>{
+    bcrypt.compare("krishi", "$2b$10$VViBFlw/MjEGcokfczWn1evjBaYdOuQhUZfyZdMROLrlwtaLXps4y", function(err, result) {
+       let token = jwt.sign({email:"krisha@gmail.com"},"secret");
+       res.cookie("token",token);
+       console.log(token);
+       res.send("done");
+    });
+    
+})
+
+app.get("/read", function (req,res) {
+    let data =jwt.verify(req.cookies.token,"secret");
+    console.log(data);
+})
+
+
+app.listen(3000);</pre>
+
+
+
 
 
 
--- app.js
+++ app.js
@@ -1,16 +1,24 @@
-const cookieParser = require('cookie-parser');
 const express = require('express');
 const app = express();
-
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 app.use(cookieParser());
 
 app.get("/",(req,res)=>{
-    res.cookie("name","krishiii");
-    res.send("done");
+    bcrypt.compare("krishi", "$2b$10$VViBFlw/MjEGcokfczWn1evjBaYdOuQhUZfyZdMROLrlwtaLXps4y", function(err, result) {
+       let token = jwt.sign({email:"krisha@gmail.com"},"secret");
+       res.cookie("token",token);
+       console.log(token);
+       res.send("done");
+    });
+    
 })
-app.get("/read",(req,res)=>{
-    console.log(req.cookies);
-    res.send("read page");
+
+app.get("/read", function (req,res) {
+    let data =jwt.verify(req.cookies.token,"secret");
+    console.log(data);
 })
+
 
 app.listen(3000);
