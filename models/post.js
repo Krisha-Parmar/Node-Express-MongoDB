62990cbd63990a41a9ac1c88379dc543382dae63
Krisha Parmar <krishaparmar710@gmail.com>
2024-07-25 18:08:37 +0530

    full-login

--- models/post.js
+++ models/post.js
@@ -1,16 +1,20 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://127.0.0.1:27017/testingthedatabase");
 
 const postSchema = mongoose.Schema({
-    postdata: String,
     user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref:'user'
+        type: mongoose.Schema.Types.ObjectId, ref:"user"
     },
     date:{
-        type: Date,
+        type:Date,
         default: Date.now
-    }
+    },
+    content: String,
+    likes:[
+        {
+            type: mongoose.Schema.Types.ObjectId, ref:"post"
+        }
+    ]
+
 
 })
 
--- models/user.js
+++ models/user.js
@@ -1,16 +1,17 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://127.0.0.1:27017/testingthedatabase");
+mongoose.connect("mongodb://127.0.0.1:27017/minipostproject");
 
 const userSchema = mongoose.Schema({
-    username: String,
-    email: String,
-    age: Number,
-    posts: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref:'post'
-        }
-    ]
+   username: String,
+   name: String,
+   age: Number,
+   email: String,
+   password: String,
+   post:[
+    {
+        type: mongoose.Schema.Types.ObjectId, ref:"post"
+    }
+   ]
 
 })
 
